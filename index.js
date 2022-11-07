74358c641e29cc00ae1acf206b5c607730722ac7
JOHN ALAFIATAYO <81050461+splashray@users.noreply.github.com>
2022-11-07 12:48:53 +0100

    ..

--- index.js
+++ index.js
@@ -27,26 +27,27 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Awuf-Booking Api ')
+});
+
 app.use('/api/auth', authRouter);
 app.use('/api/owners', ownersRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/hotels', hotelsRouter);
  
-app.get("/", (req, res) => {
-    res.send('Welcome to Awuf-Booking Api ')
-});
 
-app.use(notFound)
 app.use((err, req, res, next)=>{
-    const errorStatus = err.status || 500
-    const errorMessage = err.message || "Something went wrong!"
-    return res.status(errorStatus).json({
-        success: false,
-        status: errorStatus,
-        message: errorMessage,
-        stack: err.stack,
-    })
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Something went wrong!"
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  })
 })
+app.use(notFound)
 
 const port = config.PORT || 3000
 app.listen(port, ()=>{
