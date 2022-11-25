0defe1e131672b0152ff55b2f3d55c898101459e
JOHN ALAFIATAYO <81050461+splashray@users.noreply.github.com>
2022-11-25 15:11:49 +0100

    heroku pub

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-Parser')
+const bodyParser = require('body-parser')
 const cookieParser = require('cookie-Parser')
 const cors = require('cors')
 const config = require('./utils/config')
